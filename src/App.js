a55dbec2496ceb64a4385232a04777bc5b921256
Philip Harrell <alightedlamp@gmail.com>
2018-05-04 19:20:28 -0500

    Update packages and add a description

--- src/App.js
+++ src/App.js
@@ -11,6 +11,27 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <div className="info-bar">
+          <p>
+            These images were unknowingly inspired by Bruno Munari's &lsquo;useless
+            machines&rsquo;, which he describes in length in his wonderful book{' '}
+            <em>Design as Art</em>, and which I read after having started work on this project.
+          </p>
+          <p>In the manner of  Bruno's machines, these CSS paintings serve as a bridge between art and
+            functional design by assuming the shape and life of an abstract
+            painting -- their visual characteristics and their interactive
+            characteristics through some subtle and at times frustrating
+            interactivity -- using the tools and language of the web.
+          </p>
+          <p>
+            They don't quite push what CSS can do, but I think
+            point out some of it's foibles, which is a central theme to much of my work. Most of the images here have a
+            &lsquo;sweet spot&rsquo; or two where if you let your mouse sit, they'll take on
+            a life of their own. Compupters can be and express things of beauty
+            too, it turns out.
+          </p>
+          <p>The source files are found <a href="https://github.com/alightedlamp/CSSART.CLUB">here</a>.</p>
+        </div>
         <section className="paintings">
           {paintings.map((painting, i) => (
             <div className="painting" key={i}>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,33 +4,35 @@ class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="letter">A</div>
-        <div className="spacer" />
-        <div className="letter">P</div>
-        <div className="letter">R</div>
-        <div className="letter">O</div>
-        <div className="letter">J</div>
-        <div className="letter">E</div>
-        <div className="letter">C</div>
-        <div className="letter">T</div>
-        <div className="spacer" />
-        <div className="letter">B</div>
-        <div className="letter">Y</div>
-        <div className="spacer" />
-        <div className="letter">P</div>
-        <div className="letter">H</div>
-        <div className="letter">I</div>
-        <div className="letter">L</div>
-        <div className="letter">I</div>
-        <div className="letter">P</div>
-        <div className="spacer" />
-        <div className="letter">H</div>
-        <div className="letter">A</div>
-        <div className="letter">R</div>
-        <div className="letter">R</div>
-        <div className="letter">E</div>
-        <div className="letter">L</div>
-        <div className="letter">L</div>
+        <a href="http://philipharrell.com">
+          <div className="letter">A</div>
+          <div className="spacer" />
+          <div className="letter">P</div>
+          <div className="letter">R</div>
+          <div className="letter">O</div>
+          <div className="letter">J</div>
+          <div className="letter">E</div>
+          <div className="letter">C</div>
+          <div className="letter">T</div>
+          <div className="spacer" />
+          <div className="letter">B</div>
+          <div className="letter">Y</div>
+          <div className="spacer" />
+          <div className="letter">P</div>
+          <div className="letter">H</div>
+          <div className="letter">I</div>
+          <div className="letter">L</div>
+          <div className="letter">I</div>
+          <div className="letter">P</div>
+          <div className="spacer" />
+          <div className="letter">H</div>
+          <div className="letter">A</div>
+          <div className="letter">R</div>
+          <div className="letter">R</div>
+          <div className="letter">E</div>
+          <div className="letter">L</div>
+          <div className="letter">L</div>
+        </a>
       </footer>
     );
   }
